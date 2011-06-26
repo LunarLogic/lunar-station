b62659342355cc237f44a22c3c43da080ae49eed
Marcin Kulik <marcin.kulik@gmail.com>
2011-06-27 00:54:46 +0200

    Temporarily remove rvm from rubydev run_list

--- roles/rubydev.rb
+++ roles/rubydev.rb
@@ -1,3 +1,4 @@
 description 'LLP ruby developer workstation'
 
-run_list 'role[base]', 'recipe[rvm]', 'recipe[rvm::ruby192]', 'recipe[mysql]'
+run_list 'role[base]', 'recipe[mysql]'
+# 'recipe[rvm]', 'recipe[rvm::ruby192]', 
