6bf4362a6cad5b20cb825797aa9b63541450ab4a
Marcin Kulik <marcin.kulik@gmail.com>
2011-06-26 10:38:00 +0200

    chef-solo config

---
+++ config/solo.rb
@@ -0,0 +1,6 @@
+file_cache_path File.expand_path(File.join(File.dirname(__FILE__), '..'))
+cookbook_path File.expand_path(File.join(File.dirname(__FILE__), '..', 'cookbooks'))
+role_path File.expand_path(File.join(File.dirname(__FILE__), '..', 'roles'))
+
+# file_cache_path "/var/chef-solo"
+# cookbook_path "/var/chef-solo/cookbooks"
