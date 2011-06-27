5c06617059218cef6419c6b5a2bea7d5d419ecc6
Marcin Kulik <marcin.kulik@gmail.com>
2011-06-27 14:24:48 +0200

    Updated bootstrap script to install build tools, rvm and ruby 1.9.2

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,45 +1,69 @@
 #!/bin/bash
 
-echo -e "\e[1;34mInitializing Lunar Workstation...\e[0m"
+echo -e "\e[1;32mInitializing Lunar Workstation...\e[0m"
 
-if [ -z $1 ]; then
-  echo -e "\e[1;31mPlease specify workstation type.\e[0m"
-  exit 1
+function log {
+  echo -e "\e[1;31m>> \e[1;34m$1\e[0m"
+}
+
+if [ $OSTYPE == "linux-gnu" ]; then
+  OS="linux"
+  if [ -x /usr/bin/apt-get ]; then
+    PLATFORM="ubuntu"
+    log "Ubuntu/Debian Linux detected."
+  elif [ -x /usr/bin/yum ]; then
+    PLATFORM="fedora"
+    log "Fedora Linux detected."
+  fi
+else # TODO: check if it's "darwin*"
+  OS="osx"
+  PLATFORM="darwin"
+  log "Mac OSX detected."
 fi
 
-if [ -x /usr/bin/apt-get ]; then
-  sudo apt-get update >/dev/null 2>&1
+if [ $PLATFORM = "ubuntu" ]; then
+  log "Refreshing apt database..."
+  # sudo apt-get update >/dev/null 2>&1
 fi
 
-CHEF_SOLO_BIN=$(which chef-solo 2>/dev/null)
-GEM_BIN=$(which gem 2>/dev/null)
+log "Making sure curl is installed..."
+if [ $PLATFORM = "ubuntu" ]; then
+  sudo apt-get -y install curl
+elif [ $PLATFORM = "fedora" ]; then
+  sudo yum -y install curl
+fi
 
-if [ -z $CHEF_SOLO_BIN ]; then
-  if [ -z $GEM_BIN ]; then
-    echo -e "\e[1;32mRuby and/or Rubygems not found, installing...\e[0m"
-    if [ -x /usr/bin/yum ]; then
-      sudo yum -y install ruby ruby-devel rubygems
-    elif [ -x /usr/bin/apt-get ]; then
-      sudo apt-get -y install ruby ruby-dev rubygems
-    else
-      echo -e "\e[1;31mPlease install ruby and rubygems.\e[0m"
-      exit 1
-    fi
+log "Checking for RVM..."
+if [ ! -d ~/.rvm ]; then
+  log "RVM not found, installing..."
+  cd /tmp
+  curl -sk https://rvm.beginrescueend.com/install/rvm -o rvm-installer && chmod +x rvm-installer && ./rvm-installer --version latest || exit 1
+  cd -
+
+  log "Appending RVM function to your .bash_profile..."
+  grep "scripts/rvm" ~/.bash_profile >/dev/null 2>&1 || echo '[[ -s "$HOME/.rvm/scripts/rvm" ]] && . "$HOME/.rvm/scripts/rvm" # Load RVM function' >> ~/.bash_profile
+  . ~/.rvm/scripts/rvm
+
+  log "Installing ruby 1.9.2..."
+  if [ $PLATFORM = "ubuntu" ]; then
+    sudo apt-get -y install build-essential bison openssl libreadline6 libreadline6-dev zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev
+  elif [ $PLATFORM = "fedora" ]; then
+    sudo yum -y install gcc gcc-c++ make
   fi
 
-  echo -e "\e[1;32mInstalling chef gem...\e[0m"
-  sudo gem install chef --no-ri --no-rdoc
+  rvm install 1.9.2 && rvm use 1.9.2 --default
 fi
 
-# cd /tmp
+if [ -z $(which chef-solo 2>/dev/null) ]; then
+  log "Installing chef gem..."
+  gem install chef --no-ri --no-rdoc || exit 1
+fi
 
-RVM_SUDO_BIN=$(which rvmsudo 2>/dev/null)
-
-if [ -z $RVM_SUDO_BIN ]; then
-  SUDO_CMD=sudo
+if [ -n "$1" ]; then
+  DEV_TYPE=$1
 else
-  SUDO_CMD=rvmsudo
+  DEV_TYPE="rubydev"
 fi
 
-echo -e "\e[1;32mStarting chef-solo run...\e[0m"
-$SUDO_CMD chef-solo -c config/solo.rb -j nodes/$1.json
+log "Starting chef-solo run..."
+rvmsudo chef-solo -c config/solo.rb -j nodes/$OS-$DEV_TYPE.json
