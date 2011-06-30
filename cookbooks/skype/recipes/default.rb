b3f0b4e024cb8ccfd59b22b5ff2d7b91e097af50
Marcin Kulik <marcin.kulik@gmail.com>
2011-06-30 10:19:39 +0200

    Skype is in ubuntu partner repo, you stupid :P

--- cookbooks/skype/recipes/default.rb
+++ cookbooks/skype/recipes/default.rb
@@ -1,8 +1,7 @@
-base_url = "http://download.skype.com/linux/"
-skype_version = "2.2.0.35"
-
 case node[:platform]
 when 'fedora'
+  base_url = "http://download.skype.com/linux/"
+  skype_version = "2.2.0.35"
   filename = "skype-#{skype_version}-fedora.i586.rpm"
 
   bash "downloading package #{filename}" do
@@ -17,18 +16,7 @@ when 'fedora'
   end
 
 when 'ubuntu'
-  filename = "skype-ubuntu_#{skype_version}-1_i386.deb"
-
-  bash "downloading package #{filename}" do
-    cwd "/tmp"
-    code "curl -kLsO #{base_url}#{filename}"
-    not_if { File.exist? "/tmp/#{filename}" }
-  end
-
-  dpkg_package 'skype' do
-    source "/tmp/#{filename}"
-    action :install
-  end
+  package 'skype'
 
 when 'mac_os_x'
   dmg_package "Skype" do
