760b39aaee849dadefc9028bcac91819dd347abb
Marcin Kulik <marcin.kulik@gmail.com>
2011-10-30 21:52:15 +0100

    Fix skype recipe for Fedora

--- cookbooks/skype/recipes/default.rb
+++ cookbooks/skype/recipes/default.rb
@@ -1,9 +1,10 @@
 case node[:platform]
 when 'fedora'
   yum_repository "skype" do
-    name "Skype Repository"
+    name "skype"
     url "http://download.skype.com/linux/repos/fedora/updates/i586"
     key "http://www.skype.com/products/skype/linux/rpm-public-key.asc"
+    action :add
   end
 
   package 'skype'
