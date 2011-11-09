351289e7f962d62b80909079d809891875295ba8
Marcin Kulik <marcin.kulik@gmail.com>
2011-11-09 18:51:59 +0100

    No signing key for Fedora's Skype package

--- cookbooks/skype/recipes/default.rb
+++ cookbooks/skype/recipes/default.rb
@@ -4,7 +4,6 @@ when 'fedora'
     name "skype"
     description "Official Skype repository"
     url "http://download.skype.com/linux/repos/fedora/updates/i586"
-    key "http://www.skype.com/products/skype/linux/rpm-public-key.asc"
     action :add
   end
 
